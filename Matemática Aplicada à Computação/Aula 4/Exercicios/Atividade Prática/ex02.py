766880f00d02a83f80340c7deeffc7e0be4163a3
Emanuel Rosa Zolet <emanuelzolet1@gmail.com>
2024-06-30 20:12:10 -0300

    Adicionado novos exercicios

--- Matemática Aplicada à Computação/Aula 4/Exercicios/Atividade Prática/ex02.py
+++ Matemática Aplicada à Computação/Aula 4/Exercicios/Atividade Prática/ex02.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 # Função quadratica y=ax^2 + b * x + c
 
 
-a = -340
-b = 5700
-c = -9500
+a = 10
+b = -150
+c = 300
 xv = -b/(2*a)
 delta = b ** 2 - 4 * a * c
 yv = -delta / (4 * a)
--- Matemática Aplicada à Computação/Aula 4/Exercicios/Vetores.py
+++ Matemática Aplicada à Computação/Aula 4/Exercicios/Vetores.py
@@ -1,5 +1,6 @@
 import numpy as np;
 
+
 v = np.array ([138.40, 86.70, 90.90, 234.90, 107.70])
 w=5.10*v
 print(w)
