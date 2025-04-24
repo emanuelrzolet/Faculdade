833cff83e1721e43cbc762732ba5068836fc69b4
Emanuel Rosa Zolet <140463663+emanuelrzolet@users.noreply.github.com>
2025-04-24 09:48:04 -0300

    Add disciplina dev web

---
+++ Desenvolvimento Web Front-End/unidade2/src/app/ola-mundo/ola-mundo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ola-mundo',
+  imports: [],
+  templateUrl: './ola-mundo.component.html',
+  styleUrl: './ola-mundo.component.css'
+})
+export class OlaMundoComponent {
+
+}
